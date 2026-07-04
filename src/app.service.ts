e77c24b5372ce6eada5261bc4fddff096a00c859
Drxray911 <e.owusumante@gmail.com>
2026-07-04 01:42:44 +0000

    Replace getHello with getStatus method

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  @Get()
+getStatus() {
+  return {
+    name: 'Agent Pro API',
+    status: 'running',
+    version: '1.0.0',
+  };
 }
