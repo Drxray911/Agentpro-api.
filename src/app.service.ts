59f9ee9dcd2eb7530a88b1dd610c2f4ef88ddc5c
Drxray911 <e.owusumante@gmail.com>
2026-07-04 01:56:23 +0000

    Refactor getStatus method indentation

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  @Get()
-getStatus() {
-  return {
-    name: 'Agent Pro API',
-    status: 'running',
-    version: '1.0.0',
-  };
+  getStatus() {
+    return {
+      name: 'Agent Pro API',
+      status: 'running',
+      version: '1.0.0',
+    };
+  }
 }
